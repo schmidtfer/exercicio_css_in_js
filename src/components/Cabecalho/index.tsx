79ba12aab3b7cf7b5a1c689104b6708e80fe37b6
fernanda <fernandatqfs@hotmail.com>
2025-03-30 13:26:27 -0300

    Alteracoes

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { CabecalhoSite } from '../Cabecalho/cabecalho'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <CabecalhoSite>
     <h1>EBAC Jobs</h1>
-  </header>
+  </CabecalhoSite>
 )
 
 export default Cabecalho
